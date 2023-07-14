1c37a3624e30b019565bf702c07f8d375e76b228
JeongminYoon <100955092+JeongminYoon@users.noreply.github.com>
2023-07-14 11:33:53 +0900

    Add Default Tile Map in "InGame" Scene
    InGame 씬 추가
    기본 타일맵 inGame씬에 추가

---
+++ Assets/Personal/YJM/Obj_AutoReturn.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obj_AutoReturn : MonoBehaviour
+{
+    public float returnTime = 1f;
+
+    private void OnEnable()
+    {
+        StartCoroutine(CheckIsAlive());
+    }
+
+    private IEnumerator CheckIsAlive()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(returnTime);
+            //
+            break;
+        }
+    }
+}
